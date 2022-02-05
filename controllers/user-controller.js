9c7afcb9b9103f473fdd66ebc0cfd6633428d523
splaza28 <splaza28@gmail.com>
2022-02-04 22:33:24 -0600

    updated user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,8 +28,35 @@ const userController = {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
-    }
+    },
 
-}
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id } , body, { new: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user foudn with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user foudn with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }   
+            
+ }
+    
+
+
 
 module.exports = userController;
